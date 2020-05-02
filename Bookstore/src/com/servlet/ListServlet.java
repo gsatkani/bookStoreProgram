9b24559c9c747f4d8d6d9705da2be9db06fb95cf
Sathish G <57170450+gsatkani@users.noreply.github.com>
2020-05-02 23:17:05 +0530

    Add files via upload

---
+++ Bookstore/src/com/servlet/ListServlet.java
@@ -0,0 +1,51 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.bookDao;
+import com.model.Book;
+
+/**
+ * Servlet implementation class ListServlet
+ */
+@WebServlet("/ListServlet")
+public class ListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		bookDao dao=new bookDao();
+		ArrayList<Book> list = dao.ListBook();
+		request.setAttribute("list", list);
+
+		RequestDispatcher rd = request.getRequestDispatcher("AllBooks.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
